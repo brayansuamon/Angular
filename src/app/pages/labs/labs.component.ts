8cbd367a5078faf816bdd6214ffe49df8d48dedc
brayansuamon <brayansuamon@gmail.com>
2023-11-20 19:30:45 -0500

    added some functions with angular

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -16,4 +16,15 @@ export class LabsComponent {
     'Create Component',
     'Create Service',
   ];
+  name = 'Brayan';
+  age = '24';
+  disabled = true;
+  img = 'https://w3schools.com/howto/img_avatar.png';
+  //Private variables can not be called in the html
+
+  person = {
+    name: 'brayan',
+    age: 23,
+    img: 'https://w3schools.com/howto/img_avatar.png',
+  };
 }
