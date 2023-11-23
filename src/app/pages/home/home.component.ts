9ecc912dbc36150dc2d8295affe8032e24e882d4
brayansuamon <brayansuamon@gmail.com>
2023-11-22 19:23:35 -0500

    connecting input with function and showing changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Install Angular CLI',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Create Project',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Create Component',
+      completed: false,
+    },
+  ]);
 
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTitle = input.value;
+    this.addTask(newTitle);
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteHandler(index: number) {
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => position !== index)
+    );
+  }
 }
